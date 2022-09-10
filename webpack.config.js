0aeb63e9ba2696174ca65dd39fafa574de44daa1
Diego Botero <diego.botero@correounivalle.edu.co>
2022-09-10 18:06:47 -0500

    Using copy webpack plugin to copy assets from src folder to dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
@@ -29,6 +30,14 @@ module.exports = {
       template: path.resolve(__dirname, 'public/index.html'),
       filename: 'index.html'
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images'
+        }
+      ]
+    })
   ]
 }
